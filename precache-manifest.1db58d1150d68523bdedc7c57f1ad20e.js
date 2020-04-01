a351770f18aa3a42a692cdba644bfdeacfb62dc1
vladislav1234000 <61660936+vladislav1234000@users.noreply.github.com>
2020-04-01 17:37:20 +0300

    Add files via upload

---
+++ precache-manifest.1db58d1150d68523bdedc7c57f1ad20e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14cd2971b96537bb877bca21cc9587f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cea639356f9bd48ebea6",
+    "url": "/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "cea639356f9bd48ebea6",
+    "url": "/static/js/2.25361332.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.25361332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf8676b2de973fc2287",
+    "url": "/static/js/main.86842e40.chunk.js"
+  },
+  {
+    "revision": "a1759b27ee01299c0554",
+    "url": "/static/js/runtime-main.a34df5a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.886c0c2a5c8b08c340a41a8c31e4145e.js"
+  "/precache-manifest.1db58d1150d68523bdedc7c57f1ad20e.js"
 );
 
 self.addEventListener('message', (event) => {
